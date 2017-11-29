1c5040f848e3d0784516a9e6b7e1ccce0291b5f2
andela-mabdussalam <mariam.abdussalam@andela.com>
2017-11-29 17:29:17 +0100

    add tests

---
+++ app/__tests__/App-test.js
@@ -0,0 +1,18 @@
+/* eslint no-undef: 0 */
+import 'react-native';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from '../App';
+
+jest.mock('../navigation/RootNavigation', () => 'RootNavigation');
+
+
+it('renders the loading screen', async () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders the root without loading screen', async () => {
+  const tree = renderer.create(<App skipLoadingScreen />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
