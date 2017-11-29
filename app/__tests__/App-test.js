9190f9edb448546cd8f06e68a688ba0f59a05289
andela-mabdussalam <mariam.abdussalam@andela.com>
2017-11-29 23:25:16 +0100

    update test

--- app/__tests__/App-test.js
+++ app/__tests__/App-test.js
@@ -2,7 +2,7 @@
 import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
-import App from '../App';
+import App from '../../App';
 
 jest.mock('../navigation/RootNavigation', () => 'RootNavigation');
 
