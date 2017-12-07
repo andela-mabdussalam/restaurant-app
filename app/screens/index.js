05127c3ef668910c313bbabd631b8995f7f58ae7
andela-mabdussalam <mariam.abdussalam@andela.com>
2017-12-07 10:09:59 +0100

    update exports

--- app/screens/index.js
+++ app/screens/index.js
@@ -1,3 +1,5 @@
 export { default as HomeScreen } from './HomeScreen';
 export { default as LinksScreen } from './LinksScreen';
 export { default as SignupScreen } from './SignupScreen';
+export { default as ProductScreen } from './ProductScreen';
+export { default as ShopScreen } from './ShopScreen';
