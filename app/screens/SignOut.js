fe3a26601d38a5e72691bc47a80b1cb250290c81
andela-mabdussalam <mariam.abdussalam@andela.com>
2017-12-11 08:53:57 +0100

    update signout screen

--- app/screens/SignOut.js
+++ app/screens/SignOut.js
@@ -1,30 +1,54 @@
 import React from 'react';
 import {
-  Button,
-  Icon,
-} from 'native-base';
-import {
   Text,
-  View,
+  AsyncStorage
 } from 'react-native';
+import PropTypes from 'prop-types';
+import { Container, Content, Card, Button, Body } from 'native-base';
 import { HomeScreenStyles as styles } from '../styles/styles';
+
 
 export default class SignOut extends React.Component {
   static navigationOptions = {
-    title: 'Profile',
+    title: 'SignOut',
+    headerTitleStyle: {
+      fontFamily: 'SinkinSans-200XLight'
+    }
   };
-  handleSignupPress = () => {
+  static propTypes = {
+    navigation: PropTypes.object
+  }
+  handleCancel = () => {
     const { navigate } = this.props.navigation;
-    navigate('Home');
+    navigate('DrawerStack');
+  }
+  handleLogout =() => {
+    const { navigate } = this.props.navigation;
+    this.removeAuthToken();
+    navigate('Main');
+  }
+  removeAuthToken = async () => {
+    await AsyncStorage.removeItem('graphcoolToken');
   }
   render() {
     return (
-      <View>
-        <Button onPress={this.SignOut} color="#F7C04C" style={styles.button} block iconLeft>
-          <Icon name='md-checkmark'/>
-          <Text style={styles.buttonText}>    Sign Out</Text>
-        </Button>
-      </View>
+    <Container>
+        <Content>
+          <Card style={{ flex: 0, padding: 25 }}>
+              <Body>
+                <Text style={styles.logoutBody}>
+                  Are you sure you want to logout?
+                </Text>
+              </Body>
+              <Button onPress={this.handleLogout} color="#F7C04C" style={styles.logoutButton} block iconLeft>
+              <Text style={styles.logoutText}>    LOG OUT</Text>
+            </Button>
+            <Button onPress={this.handleCancel} color="#F7C04C" style={styles.cancelButton} block iconLeft>
+              <Text style={styles.cancelText}>    CANCEL</Text>
+            </Button>
+          </Card>
+        </Content>
+     </Container>
     );
   }
 }
