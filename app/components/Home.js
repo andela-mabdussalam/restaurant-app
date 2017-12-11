0ab7ad88c2574c7dab36b6981fe0bb659d84f12c
andela-mabdussalam <mariam.abdussalam@andela.com>
2017-12-11 08:45:53 +0100

    add home component and container

---
+++ app/components/Home.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import {
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {
+  Button,
+  Icon,
+} from 'native-base';
+import PropTypes from 'prop-types';
+import { Field } from 'redux-form';
+import { HomeScreenStyles as styles } from '../styles/styles';
+import RenderInput from '../components/RenderInput';
+
+
+const Home = ({ handleSubmit, loginUser, handleSignupPress }) =>
+<View style={styles.container}>
+  <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+    <View style={styles.main}>
+      <View style={styles.logoContainer}>
+        <Text style={styles.largeText}>Sign  In</Text>
+        <Text style={styles.logoText}>Restaurant</Text>
+      </View>
+      <Field
+        name="email"
+        component={RenderInput}
+        placeholder="E-mail"
+      />
+      <Field
+        name="password"
+        component={RenderInput}
+        placeholder="Password"
+        secureTextEntry
+      />
+      <Button onPress={handleSubmit(loginUser)} color="#F7C04C" style={styles.button} block iconLeft>
+        <Icon name='md-checkmark'/>
+        <Text style={styles.buttonText}>    Login</Text>
+      </Button>
+      <Text style={styles.forgotPassword}>
+        Forgot Password?
+      </Text>
+      <View>
+        <View style={{ flexDirection: 'row' }}>
+          <Text style={styles.forgotPassword}>Do not have account?  </Text>
+          <TouchableOpacity
+          onPress={handleSignupPress}
+          >
+            <Text style={styles.signUpText}>Sign Up here</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+    <View style={styles.helpContainer}>
+      <View style={{ flexDirection: 'row' }}>
+        <View style={styles.lineStyle}/>
+        <Text style={styles.forgotPassword}>
+          Or Log In with
+        </Text>
+        <View style={styles.lineStyle}/>
+      </View>
+    </View>
+    <View style={styles.buttonsView} >
+      <Button color="#F7C04C" style={styles.facebookLoginBtn} block iconLeft>
+          <Icon name='logo-facebook' />
+          <Text style={styles.buttonText}>  Facebook</Text>
+      </Button>
+      <Button color="#F7C04C" style={styles.googleLoginBtn} block iconLeft>
+          <Icon name='logo-google' />
+          <Text style={styles.buttonText}>  Google</Text>
+      </Button>
+    </View>
+  </ScrollView>
+</View>;
+
+Home.propTypes = {
+  handleSubmit: PropTypes.func,
+  loginUser: PropTypes.func,
+  handleSignupPress: PropTypes.func
+};
+
+export default Home;
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,27 +1,17 @@
 import React from 'react';
-import {
-  ScrollView,
-  Text,
-  TouchableOpacity,
-  View,
-  AsyncStorage
-} from 'react-native';
-import {
-  Button,
-  Icon,
-} from 'native-base';
+import { AsyncStorage } from 'react-native';
 import PropTypes from 'prop-types';
-import { reduxForm, Field } from 'redux-form';
+import { reduxForm } from 'redux-form';
 import gql from 'graphql-tag';
 import { graphql, compose } from 'react-apollo';
 import { validate } from '../utils';
-import { HomeScreenStyles as styles } from '../styles/styles';
-import RenderInput from '../components/RenderInput';
+import Home from '../components/Home';
 
 class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null,
   };
+
   static propTypes = {
     login: PropTypes.func,
     input: PropTypes.object,
@@ -38,15 +28,17 @@ class HomeScreen extends React.Component {
       const { navigate } = this.props.navigation;
       const tokenToString = response.data.authenticateUser.token.toString();
       this.storeAuthTokensLocally(tokenToString);
-      navigate('ShopScreen');
+      navigate('DrawerStack');
     } catch (e) {
-      console.log('An error occurred: ', e);
+      return ('An error occured');
     }
   }
+
   handleSignupPress = () => {
     const { navigate } = this.props.navigation;
     navigate('SignupPage');
   }
+
   storeAuthTokensLocally = async (graphcoolToken) => {
     await AsyncStorage.setItem('graphcoolToken', graphcoolToken);
   }
@@ -54,66 +46,10 @@ class HomeScreen extends React.Component {
   render() {
     const { handleSubmit } = this.props;
     return (
-      <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.main}>
-            <View style={styles.logoContainer}>
-              <Text style={styles.largeText}>Sign  In</Text>
-              <Text style={styles.logoText}>Restaurant</Text>
-            </View>
-            <Field
-              name="email"
-              component={RenderInput}
-              placeholder="E-mail"
-            />
-            <Field
-              name="password"
-              component={RenderInput}
-              placeholder="Password"
-              secureTextEntry
-            />
-            <Button onPress={handleSubmit(this.loginUser)} color="#F7C04C" style={styles.button} block iconLeft>
-              <Icon name='md-checkmark'/>
-              <Text style={styles.buttonText}>    Login</Text>
-            </Button>
-
-
-            <Text style={styles.forgotPassword}>
-              Forgot Password?
-            </Text>
-            <View>
-              <View style={{ flexDirection: 'row' }}>
-                <Text style={styles.forgotPassword}>Do not have account?  </Text>
-                <TouchableOpacity
-                onPress={this.handleSignupPress}
-                >
-                  <Text style={styles.signUpText}>Sign Up here</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <View style={{ flexDirection: 'row' }}>
-              <View style={styles.lineStyle}/>
-              <Text style={styles.forgotPassword}>
-                Or Log In with
-              </Text>
-              <View style={styles.lineStyle}/>
-            </View>
-          </View>
-          <View style={styles.buttonsView} >
-            <Button color="#F7C04C" style={styles.facebookLoginBtn} block iconLeft>
-                <Icon name='logo-facebook' />
-                <Text style={styles.buttonText}>  Facebook</Text>
-            </Button>
-            <Button color="#F7C04C" style={styles.googleLoginBtn} block iconLeft>
-                <Icon name='logo-google' />
-                <Text style={styles.buttonText}>  Google</Text>
-            </Button>
-          </View>
-        </ScrollView>
-      </View>
+      <Home
+      handleSubmit={handleSubmit}
+      loginUser={this.loginUser}
+      handleSignupPress= {this.handleSignupPress}/>
     );
   }
 }
