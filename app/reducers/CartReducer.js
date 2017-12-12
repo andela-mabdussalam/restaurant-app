32caeaf3bc9f74003591ce725c2d84191c6a4f00
andela-mabdussalam <mariam.abdussalam@andela.com>
2017-12-12 12:39:40 +0100

    update cart reducer

--- app/reducers/CartReducer.js
+++ app/reducers/CartReducer.js
@@ -1,14 +1,57 @@
 import InitialState from '../constants/InitialState';
-import { ADD_TO_CART } from '../constants/Actiontypes';
+import { ADD_TO_CART, INCREASE_ITEM_QUANTITY, DECREASE_ITEM_QUANTITY } from '../constants/Actiontypes';
+
+export const getTotal = (state) => {
+  let reduced = 0;
+  reduced = state.reduce(
+    (total, item) =>
+      total + (parseInt(item.Price, 10) * item.quantity),
+    0
+  );
+  return reduced;
+};
 
 export const CartReducer = (state = InitialState.cart, action) => {
   switch (action.type) {
     case ADD_TO_CART:
       return {
         ...state,
-        items: [...state.items, { name: action.product.Name, url: action.product.ImageUrl }]
+        items: [...state.items, action.product],
+        total: (parseInt(action.product.Price, 10) * action.product.quantity) + getTotal(state.items),
       };
+
+    case INCREASE_ITEM_QUANTITY:
+      if (action.product.quantity === 0) {
+        return {
+          ...state,
+          items: state.items.filter(element => (element.Name !== action.product.Name)),
+          total: state.total - parseInt(action.product.Price, 10)
+        };
+      }
+      return {
+        ...state,
+        items: state.items.map(element => (element.Name === action.product.Name ?
+          { ...element, quantity: action.product.quantity } : element)),
+        total: state.total + parseInt(action.product.Price, 10)
+      };
+
+    case DECREASE_ITEM_QUANTITY:
+      if (action.product.quantity === 0) {
+        return {
+          ...state,
+          items: state.items.filter(element => (element.Name !== action.product.Name)),
+          total: state.total - parseInt(action.product.Price, 10)
+        };
+      }
+      return {
+        ...state,
+        items: state.items.map(element => (element.Name === action.product.Name ?
+          { ...element, quantity: action.product.quantity } : element)),
+        total: state.total - parseInt(action.product.Price, 10)
+      };
+
     default:
       return state;
   }
 };
+
