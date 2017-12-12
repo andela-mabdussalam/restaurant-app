4f29076d77c8e1446b342eb6f27567614c70b2ae
andela-mabdussalam <mariam.abdussalam@andela.com>
2017-12-12 12:52:07 +0100

    add tests

--- app/__tests__/Screen-test.js
+++ app/__tests__/Screen-test.js
@@ -8,9 +8,11 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import configureMockStore from 'redux-mock-store';
 import { HttpLink } from 'apollo-link-http';
 import { shallow } from 'enzyme';
+import { SignupPage } from '../screens/SignupScreen';
+import { ProductScreen } from '../screens/ProductScreen';
+import { HomeScreen, SignupScreen, ShopScreen } from '../screens';
 
-import { HomeScreen, SignupScreen, ShopScreen, ProductScreen } from '../screens';
-
+jest.mock('redux-form/lib/Field', () => 'Field');
 jest.mock('../navigation/RootNavigation', () => 'RootNavigation');
 
 const navigation = {
@@ -19,10 +21,27 @@ const navigation = {
     params: { Name: 'mariam', ImageUrl: 'Helofmdsfsd' }
   }
 };
+const signupUserMutation = jest.fn();
+const items = [{
+  Name: 'mariam',
+  ImageUrl: 'gdfgds',
+  Price: 700,
+  description: 'the hello is in the world '
+}, {
+  Name: 'tope',
+  ImageUrl: 'gdfgds',
+  Price: 700,
+  description: 'the hello is in the world'
+}];
+const state = {
+  cart: {
+    items
+  }
+};
 const mockStore = configureMockStore();
 const store = mockStore({});
 const httpLink = new HttpLink();
-
+const handleSubmit = jest.fn();
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache().restore(window.__APOLLO_STATE__),
@@ -32,29 +51,35 @@ it('tests func', async () => {
   const tree = renderer.create(<Provider store={store}>
     <HomeScreen />
     </Provider>);
-  // console.log('=====', tree.getInstance());
-  // tree.getInstance().handleSignupPress();
-  // expect(tree.getInstance().handleSignupPress().toMatchSnapshot());
   expect(tree.toJSON()).toMatchSnapshot();
-  // expect(tree).toMatchSnapshot();
 });
+
 it('renders the home screen', async () => {
   const wrapper = shallow(<HomeScreen store={store}/>);
   // expect(wrapper.find('HomeScreen').instance().handleSignupPress()).equals(true);
 });
 
-// it('renders the links screen', async () => {
-//   const tree = renderer.create(<ShopScreen />).toJSON();
-//   expect(tree).toMatchSnapshot();
-// });
+it('renders the sign up page', async () => {
+  const tree = renderer.create(<SignupPage store={store}
+    handleSubmit={handleSubmit}
+    navigation={navigation}
+    signupUserMutation={signupUserMutation}
+    />);
+  expect(tree.getInstance().storeAuthTokensLocally()).toMatchSnapshot();
+  expect(tree.getInstance().handlePress()).toMatchSnapshot();
+});
 
-it('renders the links screen', async () => {
+
+it('renders the links screen here', async () => {
   const tree = renderer.create(<Provider store={store}>
   <ApolloProvider client={client}>
-  <ProductScreen navigation={navigation}/></ApolloProvider></Provider>).toJSON();
-  // expect(tree.getInstance()).toMatchSnapshot();
-  // expect(tree.getInstance().onStarRatingPress().toMatchSnapshot());
-  // expect(tree.getInstance().hideModal().toMatchSnapshot());
+    <ProductScreen
+    state={state}
+    items={items}
+    navigation={navigation}
+    />
+  </ApolloProvider>
+  </Provider>).toJSON();
   expect(tree).toMatchSnapshot();
 });
 
@@ -65,6 +90,12 @@ it('renders the signup screen', async () => {
   ).toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+it('testing things', async () => {
+  const wrapper = shallow(<SignupScreen store={store}/>);
+  expect(wrapper).toMatchSnapshot();
+});
+
 it('renders the signup screen', async () => {
   const tree = renderer.create(<Provider store={store} >
   <ApolloProvider client={client}>
@@ -73,6 +104,7 @@ it('renders the signup screen', async () => {
   </Provider>).toJSON();
   expect(tree).toMatchSnapshot();
 });
+
 it('tests nav options', async () => {
   expect(ProductScreen.navigationOptions(navigation)).toMatchSnapshot();
 });
