e6c5453d9f581036df1e09e80636a1ad51cb4a5d
andela-mabdussalam <mariam.abdussalam@andela.com>
2017-12-12 12:56:57 +0100

    add styled text component

--- app/components/StyledText.js
+++ app/components/StyledText.js
@@ -2,12 +2,58 @@ import React from 'react';
 import { Text } from 'react-native';
 import PropTypes from 'prop-types';
 
-export const StyledText = props =>
-  <Text
-  {...props}
-    style={[props.style, { fontFamily: 'SinkinSans-200XLight' }]}
-  />;
+export const StyledText = (props) => {
+  const inlineStyles = props.style || {};
+  const {
+    marginTop,
+    color,
+    marginLeft,
+    fontSize
+  } = props;
+
+  if (props.customFont) {
+    inlineStyles.fontFamily = 'SinkinSans-200XLight';
+  }
+
+  if (props.smallFont) {
+    inlineStyles.fontSize = 11;
+  }
+
+  if (props.fontSize) {
+    inlineStyles.fontSize = fontSize;
+  }
+
+  if (props.marginTop) {
+    inlineStyles.marginTop = marginTop;
+  }
+
+  if (props.marginLeft) {
+    inlineStyles.marginLeft = marginLeft;
+  }
+
+  if (props.color) {
+    inlineStyles.color = color;
+  }
+
+  return (
+    <Text
+    {...props}
+      style={inlineStyles}
+    />
+  );
+};
 
 StyledText.propTypes = {
-  style: PropTypes.object
+  style: PropTypes.object,
+  smallFont: PropTypes.bool,
+  customFont: PropTypes.bool,
+  marginTop: PropTypes.number,
+  marginLeft: PropTypes.number,
+  color: PropTypes.string,
+  fontSize: PropTypes.number
+};
+
+StyledText.defaultProps = {
+  customFont: true,
+  smallFont: true
 };
