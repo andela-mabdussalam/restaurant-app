51f1003901075e49c61b9e3de264408f021f6d5e
andela-mabdussalam <mariam.abdussalam@andela.com>
2017-12-18 17:43:41 +0100

    format file

--- app/utils/validateInput.js
+++ app/utils/validateInput.js
@@ -5,21 +5,25 @@ export const validate = (values) => {
   } else if (values.firstName.length > 15) {
     errors.firstName = 'Must be 15 characters or less';
   }
+
   if (!values.lastName) {
     errors.lastName = 'Required';
   } else if (values.lastName.length > 15) {
     errors.lastName = 'Must be 15 characters or less';
   }
+
   if (!values.email) {
     errors.email = 'Required';
   } else if (!/^.+@.+$/i.test(values.email)) {
     errors.email = 'Invalid email address';
   }
+
   if (!values.password) {
     errors.password = 'Password is required';
   } else if (values.password.length < 6) {
     errors.password = 'Password too short';
   }
+
   if (!values.passwordConfirm) {
     errors.passwordConfirm = 'Password Confirm is required';
   } else if (values.password !== values.passwordConfirm) {
